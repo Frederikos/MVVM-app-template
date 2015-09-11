185882147517794c6b66602c038e01f2f134b5fd
Frederikos <frederikos@mail.ru>
2015-09-11 18:23:28 +0300

    Update Activity to use ViewModel

--- app/src/main/java/com/test/placesapp/activity/PlacesListActivity.java
+++ app/src/main/java/com/test/placesapp/activity/PlacesListActivity.java
@@ -1,10 +1,11 @@
 package com.test.placesapp.activity;
 
+import android.databinding.BaseObservable;
 import android.databinding.DataBindingUtil;
+import android.databinding.ObservableField;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
-import android.view.View;
 
 import com.test.placesapp.R;
 import com.test.placesapp.adapters.PlacesAdapter;
@@ -18,40 +19,48 @@ import java.util.ArrayList;
 public class PlacesListActivity extends AppCompatActivity {
 
     ActivityPlacesListBinding bind;
-    PlacesAdapter placesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         bind = DataBindingUtil.setContentView(this, R.layout.activity_places_list);
-        bind.toolbar.setTitle(R.string.places_list);
-        setupRecyclerView();
-        loadPlaces();
+        bind.setViewModel(new PlacesActivityViewModel());
     }
 
-    //TODO make ViewModel to control RecycleView and ProgressBar.
-    private void loadPlaces() {
-        PlacesLoader.getInstance().getPlacesAsync(0, new PlacesLoader.LoadListener() {
-            @Override
-            public void onSuccess(ArrayList<PlaceModel> placeModels) {
-                bind.progressBar.setVisibility(View.GONE);
-                placesAdapter.setItems(placeModels);
-            }
+    public class PlacesActivityViewModel extends BaseObservable {
+        public final ObservableField<Boolean> isDataLoading = new ObservableField<>();
+        public final String title = getString(R.string.places_list);
 
-            @Override
-            public void onFailed(String errorString) {
-                bind.progressBar.setVisibility(View.GONE);
-                Utils.showErrorInSnackBar(PlacesListActivity.this, errorString);
-            }
-        });
+        PlacesAdapter placesAdapter;
+
+        public PlacesActivityViewModel() {
+            setupRecyclerView();
+            loadPlaces();
+        }
+
+        private void setupRecyclerView() {
+            placesAdapter = new PlacesAdapter(PlacesListActivity.this);
+            bind.recyclerPlaces.setLayoutManager(new LinearLayoutManager(PlacesListActivity.this));
+            bind.recyclerPlaces.setHasFixedSize(true);
+            bind.recyclerPlaces.setAdapter(placesAdapter);
+        }
+
+        private void loadPlaces() {
+            isDataLoading.set(true);
+            PlacesLoader.getInstance().getPlacesAsync(0, new PlacesLoader.LoadListener() {
+                @Override
+                public void onSuccess(ArrayList<PlaceModel> placeModels) {
+                    isDataLoading.set(false);
+                    placesAdapter.setItems(placeModels);
+                }
+
+                @Override
+                public void onFailed(String errorString) {
+                    isDataLoading.set(false);
+                    Utils.showErrorInSnackBar(PlacesListActivity.this, errorString);
+                }
+            });
+        }
     }
-
-    private void setupRecyclerView() {
-        placesAdapter = new PlacesAdapter(this);
-        bind.recyclerPlaces.setLayoutManager(new LinearLayoutManager(this));
-        bind.recyclerPlaces.setHasFixedSize(true);
-        bind.recyclerPlaces.setAdapter(placesAdapter);
-    }
-
 
 }
